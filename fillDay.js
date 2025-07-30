745a8f1e44a2d296050483580d80af8189c416c8
Eduardo Nakatsuka <eduardo.nakatsuka@gaussy.com>
2025-07-30 13:06:59 +0900

    Code cleanup and commands improved

--- fillDay.js
+++ fillDay.js
@@ -1,23 +1,3 @@
-// // Month in JavaScript is 0-indexed (January is 0, February is 1, etc), 
-// // but by using 0 as the day it will give us the last day of the prior
-// // month. So passing in 1 as the month number will return the last day
-// // of January, not February
-// function daysInMonth (month, year) {
-//   return new Date(year, month, 0).getDate();
-// }
-
-// const now = new Date();
-// const daysInMonth = daysInMonth(now.getMonth() + 1, now.getFullYear());
-
-// const monthlyTotal = now.
-
-// make start and endtime minutes a bit more random, but matching the 9-18h timeframe
-// eg: start at 9:02, end at 17:58
-// const startHour = '09';
-// const endHour = '17';
-// const startMinute = Math.floor(Math.random() * 60);
-// const endMinute = Math.floor(60 - startMinute);
-
 const startHour = '09';
 const endHour = '18';
 // const startMinute = padToTwoDigits(Math.floor(Math.random() * 26)); // 0 to 25
--- fillDays.js
+++ fillDays.js
@@ -1,30 +1,3 @@
-
-// function fillDay() {
-//   document.getElementsByName('recording_type_code_1')[0].value = 1;
-//   document.getElementsByName('recording_type_code_2')[0].value = 2;
-
-//   document.getElementById('recording_timestamp_time_1').value = '09:00';
-//   document.getElementById("recording_timestamp_time_1").setAttribute("value", '09:00');
-//   document.getElementById("recording_timestamp_hour_1").setAttribute("value", '09');
-//   document.getElementById("recording_timestamp_minute_1").setAttribute("value", '00');
-//   document.getElementById('recording_timestamp_time_1').blur();
-//   document.getElementById('recording_timestamp_time_1').click();
-//   document.getElementById('recording_timestamp_time_2').value = '18:00';
-//   document.getElementById("recording_timestamp_time_2").setAttribute("value", '18:00');
-//   document.getElementById("recording_timestamp_hour_2").setAttribute("value", '18');
-//   document.getElementById("recording_timestamp_minute_2").setAttribute("value", '00');
-//   document.getElementById('recording_timestamp_time_2').blur();
-//   document.getElementById('recording_timestamp_time_2').click();
-
-//   document.getElementsByName('request_remark_1')[0].value = 'in';
-//   document.getElementsByName('request_remark_2')[0].value = 'out';
-// }
-
-// const today = new Date();
-// const todayDay = today.getDate();
-// // inner html date is like this... if this or bigger stop!	07/30(Wed)
-// const formattedDate = `${String(today.getMonth() + 1).padStart(2, '0')}/${String(todayDay).padStart(2, '0')}(${['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][today.getDay()]})`;
-
 async function fillDays() {
   const allDays = document.querySelectorAll('.start_end_timerecord');
   const daysNotFilled = [];
@@ -33,12 +6,9 @@ async function fillDays() {
       return true; // day is filled, skip it
     }
 
-    // it can't include Rqst as well
     if (day.parentElement.innerHTML.includes('Weekday') && !day.parentElement.innerHTML.includes('[Rqst.]')) {
       daysNotFilled.push(day);
-      // return true; // day is a working day, skip it
     }
-    // daysNotFilled.push(day);
   }));  
   
   const firstDayNotFilled = daysNotFilled[0];
@@ -49,51 +19,6 @@ async function fillDays() {
   const buttonId = option.getAttribute('value');
   const hiddenButton = document.querySelector(buttonId);
   hiddenButton.click();
-
-  //   // if inner html is empty, it means the day is not filled
-  //   return day.innerHTML.trim() === '';
-  // });
-  // const firstDayNotFilled = daysNotFilled[0];
-  // if day is not weekend / holiday, click it
-
-
-
-  // const today = new Date();
-  // const todayDay = today.getDay();
-
-  // while (todayDay < day) {
-  //   const id = `button_05901758232${day}`
-  
-  //   const requestButton = document.getElementById(id);
-  //   const row = requestButton.parentElement.parentElement.parentElement.parentElement;
-  //   const isWorkingDay = row.getElementsByClassName('schedule specific-timecard_schedule')[0].innerHTML.includes('裁量労働');
-
-  //   console.log({ day, isWorkingDay });
-
-  //   if (!isWorkingDay) {
-  //     day++;
-  //     continue;
-  //   }
-
-  //   requestButton.click();
-  //   // const buttonFunction = requestButton.getAttribute('onclick');
-  //   // console.log({ buttonFunction });
-  //   // eval(buttonFunction);
-  
-  //   await new Promise(r => setTimeout(r, 2000));
-  
-  
-  //   fillDay();
-  
-  //   const submitButton = document.getElementById('button_01');
-  //   // const submitButtonFunction = requestButton.getAttribute('onclick');
-  //   // eval(submitButtonFunction);
-  //   submitButton.click();
-  
-  //   await new Promise(r => setTimeout(r, 2000));
-  
-  //   day++;
-  // }
 }
 
 fillDays();
